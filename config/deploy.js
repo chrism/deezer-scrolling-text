3c45503315e34a233c338d27499d3b44fc0eb9b5
Chris Masters <chrismasters@gmail.com>
2018-05-18 19:20:07 +0200

    working on deployment.

--- config/deploy.js
+++ config/deploy.js
@@ -19,7 +19,9 @@ module.exports = function(deployTarget) {
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
+    ENV.ghpages = {
+      gitRemoteUrl: 'git@github.com:chrism/deezer-scrolling-text.git'
+    }
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
